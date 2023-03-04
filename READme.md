a298c38801f0839efb1a48391cd92e9b202aed53
Ufomah <ufomahokit@gmail.com>
2023-03-04 20:33:06 +0000

    READ me updated

--- READme.md
+++ READme.md
@@ -1,2 +1,3 @@
 This is a tesla project
 This is a tesla app for payment
+New cars to be added 
--- accmgt.java
+++ accmgt.java
@@ -7,6 +7,10 @@ overdraft
 payday
 stocks
 crypto
+<<<<<<< HEAD
 =======
 Manager
 >>>>>>> 9be20961e6f4e363038fdb37f5a9b8b697d28697
+=======
+Transamerica Insurance
+>>>>>>> development
