971c97ab90faee82050701fe2e5c0600046e45f0
Ufomah <ufomahokit@gmail.com>
2023-03-05 16:19:08 +0000

    modified

--- accmgt.java
+++ accmgt.java
@@ -3,14 +3,9 @@ debit cards
 savings
 checklig
 overdraft
-<<<<<<< HEAD
 payday
 stocks
 crypto
-<<<<<<< HEAD
-=======
 Manager
->>>>>>> 9be20961e6f4e363038fdb37f5a9b8b697d28697
-=======
-Transamerica Insurance
->>>>>>> development
+Transamerica Insurance development
+Government bond
